9b35d15a259d9fb57c050ec3a11b3e1a06fd3d3a
Vladimir Lysov <dubaua@gmail.com>
2026-06-09 22:18:01 +0500

    fix paths try 2. every fucking time

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  base: './',
+  base: '/endless-dungeon/',
   plugins: [solidPlugin()],
   build: {
     outDir: 'docs',
